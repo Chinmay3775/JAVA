33fda3d811317509d13cc6aae1460d604eae9cc2
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-09-17 12:59:26 +0530

    Add files via upload

---
+++ JAVA programming/Multiple level inheritance/Cal_Main.java
@@ -0,0 +1,12 @@
+public class Cal_Main {
+    public static void main(String[] args) {
+        AdvCal A=new AdvCal();
+        int R1=A.add(5, 4);
+        int R2=A.sub(6, 4);
+        int R3=A.mult(2, 3);
+        double R4 = A.div(20, 5);
+        System.out.println(R1+" "+R2+" "+R3+" "+R4);
+    }
+    
+}
+ 

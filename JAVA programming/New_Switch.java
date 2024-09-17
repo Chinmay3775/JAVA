137cd61ff24f647bfcd4d2fddb4022849db7e5cc
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2024-09-17 13:04:08 +0530

    Create New_Switch.java

---
+++ JAVA programming/New_Switch.java
@@ -0,0 +1,12 @@
+class New_Switch 
+{
+    public static void main(String a[])
+    {
+        String day="Sunday";
+        switch (day) {
+            case "Sunday","Saturday"->System.out.println("8am");
+            case "Monday","Tuesday","Wednesday","Thursday","Friday"-> System.out.println("7am");
+            default ->System.out.println("invalid input");
+        }
+    }
+}
